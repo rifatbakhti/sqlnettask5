ade740413b47d3c5870726dc6cf33ed7ab9d577b
Рифат Бахтиев <rifatbakhtiev@gmail.com>
2023-01-11 22:19:56 +0400

    Secound commit

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import psycopg2
 
-conn = psycopg2.connect(database='sqlnettask4', user='postgres', password='123')
-with conn.cursor() as cur:
-
-    # Удаление таблицы
+# 0 Удаление таблицы
+def delete_db(cur):
     cur.execute("""
     DROP TABLE clients_phone
     """)
@@ -12,7 +10,8 @@ with conn.cursor() as cur:
     DROP TABLE clients
     """)
 
-    # 1.1. Функция, создающая структуру БД для ФИО + email (таблицы)
+# 1.1. Функция, создающая структуру БД для ФИО + email (таблицы)
+def create_db(cur):
     cur.execute("""
             CREATE TABLE IF NOT EXISTS clients(
                 id SERIAL PRIMARY KEY,
@@ -23,68 +22,77 @@ with conn.cursor() as cur:
                 """)
     conn.commit()
 
-    # 1.2. Функция, создающая структуру БД для телефона (таблицы)
+# 1.2. Функция, создающая структуру БД для телефона (таблицы)
     cur.execute("""
-            CREATE TABLE IF NOT EXISTS clients_phone(
-                id SERIAL PRIMARY KEY,
-                clients_id INTEGER not null references clients(id),
-                phone VARCHAR(40) UNIQUE
-                );
-                """)
+             CREATE TABLE IF NOT EXISTS clients_phone(
+                 id SERIAL PRIMARY KEY,
+                 clients_id INTEGER not null references clients(id),
+                 phone VARCHAR(40) UNIQUE
+                 );
+                 """)
     conn.commit()
 
-    # 2. Функция, позволяющая добавить нового клиента
+# 2. Функция, позволяющая добавить нового клиента
+def add_client(cur, name, surname, email, phones=None):
     cur.execute("""
-            INSERT INTO clients(name, surname, email) VALUES('Рифат', 'Бахтиев', 'rifat@gmail.com') RETURNING id, name, surname, email;
-            """)
-    # print(cur.fetchone())
+                INSERT INTO clients (name, surname, email) 
+                VALUES (%s, %s, %s);
+                """, (name, surname, email))
+    conn.commit()
 
-    # 3. Функция, позволяющая добавить телефон для существующего клиента
+# 3. Функция, позволяющая добавить телефон для существующего клиента
+def add_phone(cur, client_id, phone):
     cur.execute("""
-            INSERT INTO clients_phone(clients_id, phone) VALUES(1, '8926') RETURNING id, clients_id, phone;
-            """)
-    # print(cur.fetchone())
+                INSERT INTO clients_phone(clients_id, phone) 
+                VALUES(%s, %s);
+                """, (client_id, phone))
+    conn.commit()
 
-    # 4.1. Функция позволяющая изменить данные о клиенте
+# 4. Функция позволяющая изменить данные о клиенте
+def change_client(cur, client_id, name=None, surname=None, email=None, phones=None):
     cur.execute("""
             UPDATE clients
-            SET name = 'Иван', surname = 'Никифоров', email = 'ivan@gmail.com' RETURNING id, name, surname, email;
-            """)
-    # print(cur.fetchone())
+            SET name = %s, surname = %s, email = %s
+            WHERE id = %s;
+            """, (name, surname, email, client_id))
 
-    # 4.2. Функция позволяющая изменить данные о клиенте ТЕЛЕФОН
     cur.execute("""
             UPDATE clients_phone
-            SET phone = '8928' RETURNING id, phone;
-            """)
-    # print(cur.fetchone())
+            SET phone = %s
+            WHERE id = %s;
+            """, (phones, client_id))
 
-    # 6.1. Функция, позволяющая удалить существующего клиента
+# 5. Функция, позволяющая удалить телефон
+def delete_phone(cur, client_id, phone):
     cur.execute("""
             DELETE FROM clients_phone
-            WHERE id = 1
-                """)
+            WHERE id = %s and phone = %s;
+                """, (client_id, phone))
 
-    # 6.2. Функция, позволяющая удалить существующего клиента
+# 6. Функция, позволяющая удалить существующего клиента
+def delete_client(cur, client_id):
     cur.execute("""
             DELETE FROM clients
-            WHERE id = 1
-                """)
+            WHERE id = %s;
+                """, (client_id))
 
-    # 7.1 Функция, позволяющая найти клиента по его данным(имени, фамилии, email - у)
+# 7. Функция, позволяющая найти клиента по его данным
+def find_client(conn, name=None, surname=None, email=None, phone=None):
     cur.execute("""
-            SELECT clients.name, clients.surname, clients.email, clients_phone.phone FROM clients
-            LEFT JOIN clients_phone on clients.id=clients_phone.clients_id
-            where (name like 'Иван') AND (surname like 'Никифоров') AND (email like 'ivan@gmail.com')
-            """)
+                SELECT clients.name, clients.surname, clients.email, clients_phone.phone FROM clients
+                LEFT JOIN clients_phone on clients.id=clients_phone.clients_id
+                where (name like %s) OR (surname like %s) OR (email like %s) OR (phone like %s)
+                """, (name, surname, email, phone))
     print(cur.fetchmany(3))
 
-    # 7.2 Функция, позволяющая найти клиента по его данным(телефону)
-    cur.execute("""
-            SELECT clients.name, clients.surname, clients.email, clients_phone.phone FROM clients
-            LEFT JOIN clients_phone on clients.id=clients_phone.clients_id
-            where clients_phone.phone like '8928'
-            """)
-    # print(cur.fetchmany(3))
+conn = psycopg2.connect(database='sqlnettask4', user='postgres', password='123')
+with conn.cursor() as cur:
 
-conn.close()
+    delete_db(cur)
+    create_db(cur)
+    add_client(cur, 'Рифат', 'Бахтиев', 'rifat@gmail.com')
+    add_phone(cur, 1, '8926')
+    change_client(cur, 1, "Иван", "Иванов", "ivan@gmail.com", "8999")
+    delete_phone(cur, 1, "8999")
+    delete_client(cur, '1')
+    find_client(cur, "Иван", "")
